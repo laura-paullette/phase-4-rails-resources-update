ea6b546d94ae1aa62131e867b1fe94d72f7a644a
laura-paullette <laura.paullette@student.moringaschool.com>
2023-06-21 00:25:08 +0300

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -28,4 +28,21 @@ class BirdsController < ApplicationController
     params.permit(:name, :species)
   end
 
+  # PATCH /birds/:id
+  def update
+  #   bird = Bird.find_by(id: params[:id])
+  #   if bird
+  #     bird.update(bird_params)
+  #     render json: bird
+  #   else
+  #     render json: { error: "Bird not found" }, status: :not_found
+  #   end
+  # end
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+  else
+    render json: { error: "Bird not found" }, status: :not_found
+  end
 end
